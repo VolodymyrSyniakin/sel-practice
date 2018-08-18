75797c28566cf28953ea06e983c9bb2041025c0e
VolodymyrSyniakin <Vsyniakin@gmail.com>
2018-08-18 12:50:25 +0300

    add tasks by week 2

---
+++ src/main/java/com/gmail/vsyniakin/AdminDashboardPage.java
@@ -0,0 +1,102 @@
+package com.gmail.vsyniakin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class AdminDashboardPage {
+	private final WebDriver driver;
+	
+	private final String regexURL = ".*AdminDashboard.*";
+	
+	private static By getOutLoc = By.id("header_logout");
+	
+	private static By emplInfLoc = By.id("employee_infos");
+	
+	private static By dashboardLinkLoc = By.linkText("Dashboard");
+	
+	private static By mainTabLoc = By.className("maintab");
+	
+	private static By titleLinkLoc = By.className("title");
+
+	public AdminDashboardPage(WebDriver driver) {
+		super();
+		
+		WebElement dashboardLinkEl = driver.findElement(dashboardLinkLoc);
+		if (dashboardLinkEl!=null) {
+			if (!driver.getCurrentUrl().matches(regexURL)) {
+				throw new IllegalStateException("This is not AdminDashboardPage!");
+			}
+		}
+		this.driver = driver;
+	}
+	
+	public AdminLoginPage getOut () {
+		WebElement emplInfEl = driver.findElement(emplInfLoc);
+		emplInfEl.click();
+		
+		WebElement getOutEl = driver.findElement(getOutLoc);
+		getOutEl.click();
+		
+		return new AdminLoginPage(driver);
+	}
+	
+	public List<String> getIdMainLi () {
+		List<WebElement> mainTab = driver.findElements(mainTabLoc);
+		List<String> idLi = new ArrayList<String>();
+		for (WebElement element : mainTab) {
+			try {
+				idLi.add(element.getAttribute("id"));
+			} catch (NoSuchElementException e) {}
+		}
+		return idLi;
+	}
+	
+	public static By getGetOutLoc() {
+		return getOutLoc;
+	}
+
+	public static void setGetOutLoc(By getOutLoc) {
+		AdminDashboardPage.getOutLoc = getOutLoc;
+	}
+
+	public static By getEmplInfLoc() {
+		return emplInfLoc;
+	}
+
+	public static void setEmplInfLoc(By emplInfLoc) {
+		AdminDashboardPage.emplInfLoc = emplInfLoc;
+	}
+
+	public static By getDashboardLinkLoc() {
+		return dashboardLinkLoc;
+	}
+
+	public static void setDashboardLinkLoc(By dashboardLinkLoc) {
+		AdminDashboardPage.dashboardLinkLoc = dashboardLinkLoc;
+	}
+
+	public static By getMainTabLoc() {
+		return mainTabLoc;
+	}
+
+	public static void setMainTabLoc(By mainTabLoc) {
+		AdminDashboardPage.mainTabLoc = mainTabLoc;
+	}
+
+	public static By getTitleLinkLoc() {
+		return titleLinkLoc;
+	}
+
+	public static void setTitleLinkLoc(By titleLinkLoc) {
+		AdminDashboardPage.titleLinkLoc = titleLinkLoc;
+	}
+
+
+	
+	
+}
---
+++ src/main/java/com/gmail/vsyniakin/PracticeOne.java
@@ -0,0 +1,84 @@
+package com.gmail.vsyniakin;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class PracticeOne {
+	
+	public static void main(String[] args) {
+		WebDriver driver = initFirefoxDriver("src/main/resources/geckodriver.exe");
+		driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
+		setTimeOuts(driver, 5);
+		
+// task 1:		
+		signInAndOut(driver);
+		
+// task 2:
+		clickAllIntoMenuDashboard(driver);
+		
+		driver.close();
+	}
+	
+/**Логин в Админ панель:
+* 	1. Открыть страницу Админ панели
+*	2. Ввести логин, пароль и нажать кнопку Логин.
+*	3. После входа в систему нажать на пиктограмме пользователя в верхнем правом углу и выбрать опцию «Выход.»
+* */
+	public static void signInAndOut(WebDriver driver) {
+		AdminLoginPage loginPage = new AdminLoginPage(driver);
+		AdminDashboardPage adminDashPage = loginPage.submitLogin("webinar.test@gmail.com", "Xcg7299bnSmMuRLp9ITw");
+		loginPage = adminDashPage.getOut();
+	}
+/**Проверка работоспособности главного меню Админ панели: 
+*   1. Войти в Админ панель (по аналогии с предыдущим скриптом)
+*	2. Кликнуть на каждом видимом пункте главного меню (Dashboard, Заказы, Каталог, Клиенты…) для открытия соответствующего раздела и выполнить следующее:
+*		a. Вывести в консоль заголовок открытого раздела.
+*		b. Выполнить обновление (рефреш) страницы и проверить, что пользователь остается в том же разделе после перезагрузки страницы.
+* */	
+	public static void clickAllIntoMenuDashboard (WebDriver driver) {
+		AdminLoginPage loginPage = new AdminLoginPage(driver);
+		AdminDashboardPage adminDashPage = loginPage.submitLogin("webinar.test@gmail.com", "Xcg7299bnSmMuRLp9ITw");
+		List<String> idLinks = adminDashPage.getIdMainLi();
+		for (String id : idLinks) {
+			driver = clickLinkMainMenu(id, driver);
+			
+			String title = driver.getTitle();
+			System.out.println(title);
+			
+			driver.navigate().refresh();
+
+			if (!title.equals(driver.getTitle())) {
+				System.err.println("Test for refresh page - fail, by url: " + driver.getCurrentUrl());
+			}
+		}
+	}
+	
+	public static WebDriver clickLinkMainMenu(String id, WebDriver driver) {
+		try {
+			driver.findElement(By.id(id)).findElement(AdminDashboardPage.getTitleLinkLoc()).click();
+		} catch (NoSuchElementException e) {
+			System.err.println("Next element by id=" + id + "; " + "not found! " + "on url: " + driver.getCurrentUrl());
+			driver.navigate().back();
+			return clickLinkMainMenu(id, driver);
+		}
+		return driver;
+	}
+	
+	public static WebDriver initFirefoxDriver (String path) {
+		System.setProperty("webdriver.gecko.driver", path);
+		return new FirefoxDriver();
+	}
+	
+	public static WebDriver setTimeOuts (WebDriver driver, long sec) {
+		driver.manage().timeouts().implicitlyWait(sec, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(sec, TimeUnit.SECONDS);
+		driver.manage().timeouts().setScriptTimeout(sec, TimeUnit.SECONDS);
+		return driver;
+	}
+}
